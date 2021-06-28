de0a8986827667f7840a075e5db11055fca090c5
eaty <eaty>
2021-06-28 19:31:06 +0800

    Initial Commit

---
+++ final1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  final1
+//
+//  Created by Eat Myself on 2021/6/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ final1/final1App.swift
@@ -0,0 +1,17 @@
+//
+//  final1App.swift
+//  final1
+//
+//  Created by Eat Myself on 2021/6/28.
+//
+
+import SwiftUI
+
+@main
+struct final1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
